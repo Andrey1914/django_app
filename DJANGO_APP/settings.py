20bab7eab9c51270f44e0a3cb49a266495950ca8
Andrey1914 <anlikajw@gmail.com>
2024-06-19 22:33:33 +0200

    added templates

--- DJANGO_APP/settings.py
+++ DJANGO_APP/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'DJANGO_PROJECT',
+    'NEW_YEAR',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DJANGO_APP/urls.py
+++ DJANGO_APP/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("DJANGO_PROJECT.urls"))
+    path('hello/', include("DJANGO_PROJECT.urls")),
+    path('newyear/', include("NEW_YEAR.urls"))
 ]
