dcd66a9652bf60128bcd57558068771b6073d5e8
Andrey1914 <anlikajw@gmail.com>
2024-06-21 13:36:39 +0200

    created a tasks project

--- DJANGO_APP/settings.py
+++ DJANGO_APP/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'DJANGO_PROJECT',
     'NEW_YEAR',
+    'TASKS',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DJANGO_APP/urls.py
+++ DJANGO_APP/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("DJANGO_PROJECT.urls")),
-    path('newyear/', include("NEW_YEAR.urls"))
+    path('newyear/', include("NEW_YEAR.urls")),
+    path('tasks/', include("TASKS.urls"))
 ]
