7853a596f055af53193e622a43a51386932403f1
Andrey1914 <anlikajw@gmail.com>
2024-06-21 16:21:52 +0200

    added session and migrated

--- TASKS/views.py
+++ TASKS/views.py
@@ -3,12 +3,19 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-tasks = ["foo", "bar", "baz"]
 # Create your views here.
 
 def index(request):
+
+    # Check if there already exists a "tasks" key in our session
+
+    if "tasks" not in request.session:
+
+        # If not, create a new list
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 # Add a new task:
@@ -33,7 +40,7 @@ def add(request):
             task = form.cleaned_data["task"]
 
             # Add the new task to our list of tasks
-            tasks.append(task)
+            request.session["tasks"] += [task]
 
             # Redirect user to list of tasks
             return HttpResponseRedirect(reverse("tasks:index"))
